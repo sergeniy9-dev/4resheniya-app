6e00710d9cf4d0c9a65ed7aedbb83e7eec063873
serg last <sergeniy9@gmail.com>
2026-05-10 22:18:42 +0300

    Add premium PWA icons

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+
+      manifest: {
+        name: "4РЕШЕНИЯ",
+        short_name: "4РЕШЕНИЯ",
+
+        description:
+          "Интерактивная карта сценариев пространства",
+
+        theme_color: "#050505",
+        background_color: "#050505",
+
+        display: "standalone",
+
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
